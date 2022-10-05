071b8d5857f291748980a033ffe0a7f3c31bb16b
Shane Vitarana <shanev@gmail.com>
2022-10-05 17:03:52 -0400

    Validate name

--- contracts/name-minter/src/contract.rs
+++ contracts/name-minter/src/contract.rs
@@ -22,6 +22,9 @@ const CONTRACT_VERSION: &str = env!("CARGO_PKG_VERSION");
 
 const INIT_COLLECTION_REPLY_ID: u64 = 1;
 
+const MIN_NAME_LENGTH: u64 = 3;
+const MAX_NAME_LENGTH: u64 = 63;
+
 #[cfg_attr(not(feature = "library"), entry_point)]
 pub fn instantiate(
     deps: DepsMut,
@@ -94,13 +97,16 @@ pub fn execute_mint_and_list(
     // TODO: add to governance
     let BASE_PRICE = 100000000u128;
 
-    let count = name.graphemes(true).count();
-    let amount = match count {
-        1 => return Err(ContractError::NameTooShort {}),
-        2 => return Err(ContractError::NameTooShort {}),
+    if !name.is_ascii() {
+        return Err(ContractError::InvalidName {});
+    }
+
+    // Because we know we are left with ASCII chars, a simple byte count is enough
+    let amount = match name.len() {
+        0..2 => return Err(ContractError::NameTooShort {}),
         3 => BASE_PRICE * 100,
         4 => BASE_PRICE * 10,
-        5..255 => BASE_PRICE,
+        5..63 => BASE_PRICE,
         _ => return Err(ContractError::NameTooLong {}),
     };
     let price = coin(amount, "ustars");
@@ -132,6 +138,50 @@ pub fn execute_mint_and_list(
     Ok(Response::new()
         .add_attribute("action", "mint")
         .add_message(msg))
+}
+
+// We store the name without the TLD so it can be mapped to a raw address
+// that is not bech32 encoded. This way, all Cosmos / Interchain names can
+// be resolved to an address that is derived via the same (118) derivation
+// path.
+//
+// For example:
+//
+// bobo -> D93385094E906D7DA4EBFDEC2C4B167D5CAA431A (in hex)
+//
+// Now this can be resolved per chain:
+//
+// bobo.stars -> stars1myec2z2wjpkhmf8tlhkzcjck04w25sc6ymhplz
+// bobo.cosmos -> cosmos1myec2z2wjpkhmf8tlhkzcjck04w25sc6y2xq2r
+fn validate_name(name: &str) -> Result<(), ContractError> {
+    let len = name.len() as u64;
+    if len < MIN_NAME_LENGTH {
+        return Err(ContractError::NameTooShort {});
+    } else if len > MAX_NAME_LENGTH {
+        return Err(ContractError::NameTooLong {});
+    }
+
+    name.find(invalid_char)
+        .map_or(Ok(()), |_| Err(ContractError::InvalidName {}));
+
+    name.starts_with("-")
+        .then(|| Err(ContractError::InvalidName {}));
+
+    name.ends_with("-")
+        .then(|| Err(ContractError::InvalidName {}));
+
+    if len > 4 {
+        name[2..]
+            .find("--")
+            .map_or(Ok(()), |_| Err(ContractError::InvalidName {}))
+    }
+
+    Ok(())
+}
+
+fn invalid_char(c: char) -> bool {
+    let is_valid = c.is_digit(10) || c.is_ascii_lowercase() || (c == '-');
+    !is_valid
 }
 
 #[cfg_attr(not(feature = "library"), entry_point)]
