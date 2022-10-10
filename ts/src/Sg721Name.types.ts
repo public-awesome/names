f117bac43b2bb68f3d05a71356ff8983a6afad3f
Shane Vitarana <shanev@gmail.com>
2022-10-09 20:08:54 -0400

    Update schema and types for sg721-name

--- ts/src/Sg721Name.types.ts
+++ ts/src/Sg721Name.types.ts
@@ -4,32 +4,163 @@
 * and run the @cosmwasm/ts-codegen generate command to regenerate this file.
 */
 
-export interface CountResponse {
-  count: number;
+export type Expiration = {
+  at_height: number;
+} | {
+  at_time: Timestamp;
+} | {
+  never: {};
+};
+export type Timestamp = Uint64;
+export type Uint64 = string;
+export type Addr = string;
+export interface AllNftInfoResponse {
+  access: OwnerOfResponse;
+  info: NftInfoResponseForMetadataForEmpty;
+}
+export interface OwnerOfResponse {
+  approvals: Approval[];
+  owner: string;
+}
+export interface Approval {
+  expires: Expiration;
+  spender: string;
+}
+export interface NftInfoResponseForMetadataForEmpty {
+  extension: MetadataForEmpty;
+  token_uri?: string | null;
+}
+export interface MetadataForEmpty {
+  bio?: string | null;
+  extension: Empty;
+  profile?: NFT | null;
+  records: TextRecord[];
   [k: string]: unknown;
 }
-export type ExecuteMsg = {
-  increment: {
-    [k: string]: unknown;
-  };
-} | {
-  reset: {
-    count: number;
-    [k: string]: unknown;
-  };
-};
-export interface InstantiateMsg {
-  count: number;
+export interface Empty {
   [k: string]: unknown;
+}
+export interface NFT {
+  collection: Addr;
+  token_id: string;
+  [k: string]: unknown;
+}
+export interface TextRecord {
+  name: string;
+  value: string;
+  verified_at?: Timestamp | null;
+  [k: string]: unknown;
+}
+export interface AllOperatorsResponse {
+  operators: Approval[];
+}
+export interface AllTokensResponse {
+  tokens: string[];
+}
+export interface ApprovalResponse {
+  approval: Approval;
+}
+export interface ApprovalsResponse {
+  approvals: Approval[];
+}
+export type Decimal = string;
+export interface CollectionInfoResponse {
+  creator: string;
+  description: string;
+  explicit_content: boolean;
+  external_link?: string | null;
+  image: string;
+  royalty_info?: RoyaltyInfoResponse | null;
+  trading_start_time?: Timestamp | null;
+}
+export interface RoyaltyInfoResponse {
+  payment_address: string;
+  share: Decimal;
+}
+export interface ContractInfoResponse {
+  name: string;
+  symbol: string;
+}
+export interface InstantiateMsg {
+  collection_info: CollectionInfoForRoyaltyInfoResponse;
+  minter: string;
+  name: string;
+  symbol: string;
+}
+export interface CollectionInfoForRoyaltyInfoResponse {
+  creator: string;
+  description: string;
+  explicit_content: boolean;
+  external_link?: string | null;
+  image: string;
+  royalty_info?: RoyaltyInfoResponse | null;
+  trading_start_time?: Timestamp | null;
+}
+export interface MinterResponse {
+  minter: string;
+}
+export interface NftInfoResponse {
+  extension: MetadataForEmpty;
+  token_uri?: string | null;
+}
+export interface NumTokensResponse {
+  count: number;
+}
+export interface OperatorsResponse {
+  operators: Approval[];
 }
 export type QueryMsg = {
-  get_count: {
-    [k: string]: unknown;
+  owner_of: {
+    include_expired?: boolean | null;
+    token_id: string;
   };
+} | {
+  approval: {
+    include_expired?: boolean | null;
+    spender: string;
+    token_id: string;
+  };
+} | {
+  approvals: {
+    include_expired?: boolean | null;
+    token_id: string;
+  };
+} | {
+  all_operators: {
+    include_expired?: boolean | null;
+    limit?: number | null;
+    owner: string;
+    start_after?: string | null;
+  };
+} | {
+  num_tokens: {};
+} | {
+  contract_info: {};
+} | {
+  nft_info: {
+    token_id: string;
+  };
+} | {
+  all_nft_info: {
+    include_expired?: boolean | null;
+    token_id: string;
+  };
+} | {
+  tokens: {
+    limit?: number | null;
+    owner: string;
+    start_after?: string | null;
+  };
+} | {
+  all_tokens: {
+    limit?: number | null;
+    start_after?: string | null;
+  };
+} | {
+  minter: {};
+} | {
+  collection_info: {};
 };
-export type Addr = string;
-export interface State {
-  count: number;
-  owner: Addr;
-  [k: string]: unknown;
+export interface TokensResponse {
+  tokens: string[];
 }
