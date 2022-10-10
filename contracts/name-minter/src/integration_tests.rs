f9ff2206bb05cd436ba4da2af2e7c272b1d5c595
John Y <j@publicawesome.com>
2022-10-09 21:54:45 -0400

    mv profile test to integration tests

--- contracts/name-minter/src/integration_tests.rs
+++ contracts/name-minter/src/integration_tests.rs
@@ -522,3 +522,249 @@ mod query {
         assert_eq!(res.queue[0], "hack".to_string());
     }
 }
+
+mod profile {
+    // requires a lot of boilerplate from vending-factory, vending-minter, sg721 to set up the test scenario
+    use super::*;
+
+    use cosmwasm_std::{coin, Timestamp};
+    use cw721_base::Extension;
+    use sg2::{msg::Sg2ExecuteMsg, tests::mock_collection_params};
+    use sg721::{ExecuteMsg as Sg721ExecuteMsg, MintMsg};
+    use sg_name::{Metadata, NFT};
+    use sg_std::{StargazeMsgWrapper, GENESIS_MINT_START_TIME, NATIVE_DENOM};
+    use vending_factory::{
+        msg::{VendingMinterCreateMsg, VendingMinterInitMsgExtension},
+        state::{ParamsExtension, VendingMinterParams},
+    };
+    use vending_minter::msg::{ConfigResponse, QueryMsg};
+
+    pub fn contract_factory() -> Box<dyn Contract<StargazeMsgWrapper>> {
+        let contract = ContractWrapper::new(
+            vending_factory::contract::execute,
+            vending_factory::contract::instantiate,
+            vending_factory::contract::query,
+        );
+        Box::new(contract)
+    }
+
+    pub fn contract_minter() -> Box<dyn Contract<StargazeMsgWrapper>> {
+        let contract = ContractWrapper::new(
+            vending_minter::contract::execute,
+            vending_minter::contract::instantiate,
+            vending_minter::contract::query,
+        )
+        .with_reply(vending_minter::contract::reply);
+        Box::new(contract)
+    }
+
+    pub fn contract_sg721() -> Box<dyn Contract<StargazeMsgWrapper>> {
+        let contract = ContractWrapper::new(
+            sg721_base::entry::execute,
+            sg721_base::entry::instantiate,
+            sg721_base::entry::query,
+        );
+        Box::new(contract)
+    }
+
+    const CREATION_FEE: u128 = 5_000_000_000;
+    const INITIAL_BALANCE: u128 = 2_000_000_000;
+    const MINT_PRICE: u128 = 100_000_000;
+    const MAX_TOKEN_LIMIT: u32 = 10000;
+
+    pub const MIN_MINT_PRICE: u128 = 50_000_000;
+    pub const AIRDROP_MINT_PRICE: u128 = 0;
+    pub const MINT_FEE_BPS: u64 = 1_000; // 10%
+    pub const AIRDROP_MINT_FEE_BPS: u64 = 10_000; // 100%
+    pub const SHUFFLE_FEE: u128 = 500_000_000;
+    pub const MAX_PER_ADDRESS_LIMIT: u32 = 50;
+
+    pub fn mock_params() -> VendingMinterParams {
+        VendingMinterParams {
+            code_id: 1,
+            creation_fee: coin(CREATION_FEE, NATIVE_DENOM),
+            min_mint_price: coin(MIN_MINT_PRICE, NATIVE_DENOM),
+            mint_fee_bps: MINT_FEE_BPS,
+            max_trading_offset_secs: 60 * 60 * 24 * 7,
+            extension: ParamsExtension {
+                max_token_limit: MAX_TOKEN_LIMIT,
+                max_per_address_limit: MAX_PER_ADDRESS_LIMIT,
+                airdrop_mint_price: coin(AIRDROP_MINT_PRICE, NATIVE_DENOM),
+                airdrop_mint_fee_bps: AIRDROP_MINT_FEE_BPS,
+                shuffle_fee: coin(SHUFFLE_FEE, NATIVE_DENOM),
+            },
+        }
+    }
+
+    pub fn mock_init_extension(splits_addr: Option<String>) -> VendingMinterInitMsgExtension {
+        VendingMinterInitMsgExtension {
+            base_token_uri: "ipfs://aldkfjads".to_string(),
+            payment_address: splits_addr,
+            start_time: Timestamp::from_nanos(GENESIS_MINT_START_TIME),
+            num_tokens: 100,
+            mint_price: coin(MIN_MINT_PRICE, NATIVE_DENOM),
+            per_address_limit: 5,
+            whitelist: None,
+        }
+    }
+
+    pub fn mock_create_minter(splits_addr: Option<String>) -> VendingMinterCreateMsg {
+        VendingMinterCreateMsg {
+            init_msg: mock_init_extension(splits_addr),
+            collection_params: mock_collection_params(),
+        }
+    }
+
+    // Upload contract code and instantiate minter contract
+    fn setup_minter_contract(
+        router: &mut StargazeApp,
+        creator: &Addr,
+        num_tokens: u32,
+        splits_addr: Option<String>,
+    ) -> (Addr, ConfigResponse) {
+        let minter_code_id = router.store_code(contract_minter());
+        println!("minter_code_id: {}", minter_code_id);
+        let creation_fee = coins(CREATION_FEE, NATIVE_DENOM);
+
+        let factory_code_id = router.store_code(contract_factory());
+        println!("factory_code_id: {}", factory_code_id);
+
+        let mut params = mock_params();
+        params.code_id = minter_code_id;
+
+        let factory_addr = router
+            .instantiate_contract(
+                factory_code_id,
+                creator.clone(),
+                &vending_factory::msg::InstantiateMsg { params },
+                &[],
+                "factory",
+                None,
+            )
+            .unwrap();
+
+        let sg721_code_id = router.store_code(contract_sg721());
+        println!("sg721_code_id: {}", sg721_code_id);
+
+        let mut msg = mock_create_minter(splits_addr);
+        msg.init_msg.mint_price = coin(MINT_PRICE, NATIVE_DENOM);
+        msg.init_msg.num_tokens = num_tokens;
+        msg.collection_params.code_id = sg721_code_id;
+        msg.collection_params.info.creator = creator.to_string();
+
+        let msg = Sg2ExecuteMsg::CreateMinter(msg);
+
+        let res = router.execute_contract(creator.clone(), factory_addr, &msg, &creation_fee);
+        assert!(res.is_ok());
+
+        // could get the minter address from the response above, but we know its contract1
+        let minter_addr = Addr::unchecked("contract1");
+
+        let config: ConfigResponse = router
+            .wrap()
+            .query_wasm_smart(minter_addr.clone(), &QueryMsg::Config {})
+            .unwrap();
+
+        (minter_addr, config)
+    }
+    fn setup_accounts(router: &mut StargazeApp) -> (Addr, Addr) {
+        let buyer = Addr::unchecked(USER);
+        let creator = Addr::unchecked("creator");
+        // 3,000 tokens
+        let creator_funds = coins(INITIAL_BALANCE + CREATION_FEE, NATIVE_DENOM);
+        // 2,000 tokens
+        let buyer_funds = coins(INITIAL_BALANCE, NATIVE_DENOM);
+        router
+            .sudo(CwSudoMsg::Bank({
+                BankSudo::Mint {
+                    to_address: creator.to_string(),
+                    amount: creator_funds.clone(),
+                }
+            }))
+            .map_err(|err| println!("{:?}", err))
+            .ok();
+
+        router
+            .sudo(CwSudoMsg::Bank({
+                BankSudo::Mint {
+                    to_address: buyer.to_string(),
+                    amount: buyer_funds.clone(),
+                }
+            }))
+            .map_err(|err| println!("{:?}", err))
+            .ok();
+
+        // Check native balances
+        let creator_native_balances = router.wrap().query_all_balances(creator.clone()).unwrap();
+        assert_eq!(creator_native_balances, creator_funds);
+
+        // Check native balances
+        let buyer_native_balances = router.wrap().query_all_balances(buyer.clone()).unwrap();
+        assert_eq!(buyer_native_balances, buyer_funds);
+
+        (creator, buyer)
+    }
+
+    // Set blockchain time to after mint by default
+    fn setup_block_time(router: &mut StargazeApp, nanos: u64, height: Option<u64>) {
+        let mut block = router.block_info();
+        block.time = Timestamp::from_nanos(nanos);
+        if let Some(h) = height {
+            block.height = h;
+        }
+        router.set_block(block);
+    }
+
+    #[test]
+    fn update_profile() {
+        let mut app = instantiate_contracts();
+
+        let mut router = custom_mock_app();
+        let (creator, user) = setup_accounts(&mut router);
+        let num_tokens = 2;
+        let (minter_addr, config) = setup_minter_contract(&mut router, &creator, num_tokens, None);
+
+        let token_id = "king arthur".to_string();
+        let mint_msg = MintMsg::<Extension> {
+            token_id: token_id.to_string(),
+            owner: user.to_string(),
+            token_uri: None,
+            extension: None,
+        };
+
+        setup_block_time(&mut router, GENESIS_MINT_START_TIME + 1, None);
+        let exec_msg = Sg721ExecuteMsg::Mint(mint_msg.clone());
+        router
+            .execute_contract(
+                creator.clone(),
+                minter_addr.clone(),
+                &exec_msg,
+                &coins(MINT_PRICE, NATIVE_DENOM),
+            )
+            .unwrap();
+
+        mint_and_list(&mut app, NAME, USER);
+
+        // update profile
+        let profile = Some(NFT {
+            collection: Addr::unchecked(config.sg721_address),
+            token_id,
+        });
+        let msg = Sg721NameExecuteMsg::UpdateProfile {
+            name: NAME.to_string(),
+            profile,
+        };
+
+        let res = router
+            .execute_contract(
+                Addr::unchecked(USER),
+                Addr::unchecked("contract2".to_string()),
+                &msg,
+                &[],
+            )
+            .unwrap_err();
+        println!("{:?}", res);
+        assert!(false);
+        // assert!(res.is_ok());
+    }
+}
