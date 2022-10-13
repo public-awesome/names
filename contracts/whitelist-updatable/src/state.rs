6e6744c0f1a2a6b997db42fd5a07a144282f134b
John Y <j@publicawesome.com>
2022-10-13 13:56:55 -0500

    add state

--- contracts/whitelist-updatable/src/state.rs
+++ contracts/whitelist-updatable/src/state.rs
@@ -1,1 +1,13 @@
+use cosmwasm_schema::cw_serde;
+use cosmwasm_std::Addr;
+use cw_storage_plus::{Item, Map};
 
+#[cw_serde]
+pub struct Config {
+    pub admin: Addr,
+    pub per_address_limit: u32,
+}
+
+pub const TOTAL_ADDRESS_COUNT: Item<u64> = Item::new("total_address_count");
+// Holds all addresses and mint count
+pub const WHITELIST: Map<Addr, u64> = Map::new("wl");
