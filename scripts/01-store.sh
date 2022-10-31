4cf15b1188bbf3d590362259a5327b2754d2c30f
shane.stars <shanev@gmail.com>
2022-10-30 21:40:27 -0400

    Fixed scripts (#134)
    * Fixed scripts
    
    * Add download script

--- scripts/01-store.sh
+++ scripts/01-store.sh
@@ -1,3 +1,13 @@
+curl -s https://api.github.com/repos/public-awesome/names/releases/latest \
+| grep ".*wasm" \
+| cut -d : -f 2,3 \
+| tr -d \" \
+| wget -qi -
+
+starsd config node $NODE
+starsd config chain-id $CHAIN_ID
+starsd config output json
+
 starsd tx wasm store name_marketplace.wasm --from $TESTNET_KEY \
     --gas-prices 0.025ustars --gas-adjustment 1.7 \
     --gas auto -y -b block -o json | jq '.logs' | grep -A 1 code_id
