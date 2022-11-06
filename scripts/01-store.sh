c6d98fee5eed241a2a8b8c6c22b7775e8e315d15
Shane Vitarana <shanev@gmail.com>
2022-11-06 20:22:03 +0000

    Add instantitate-only-address

--- scripts/01-store.sh
+++ scripts/01-store.sh
@@ -9,10 +9,12 @@ starsd config chain-id $CHAIN_ID
 starsd config output json
 
 starsd tx wasm store name_marketplace.wasm --from $TESTNET_KEY \
+    --instantiate-only-address $ADMIN \
     --gas-prices 0.025ustars --gas-adjustment 1.7 \
     --gas auto -y -b block -o json | jq '.logs' | grep -A 1 code_id
 
 starsd tx wasm store name_minter.wasm --from $TESTNET_KEY \
+    --instantiate-only-address $ADMIN \
     --gas-prices 0.025ustars --gas-adjustment 1.7 \
     --gas auto -y -b block -o json | jq '.logs' | grep -A 1 code_id
 
