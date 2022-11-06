8ce6b09e4564a5b0d79b8a65d06c7d357272e0d3
John Y <j@publicawesome.com>
2022-11-06 12:58:41 -0500

    use random name for minting

--- scripts/exec_mint.sh
+++ scripts/exec_mint.sh
@@ -10,19 +10,20 @@ EOF
 starsd tx wasm execute $COLLECTION "$MSG" \
   --gas-prices 0.025ustars --gas auto --gas-adjustment 1.9 \
   --from $TESTNET_KEY -b block -y -o json | jq .
- 
 
+# generate random 20 char string for name
+name=$(openssl rand -hex 20);
 MSG=$(cat <<EOF
 {
   "mint_and_list": {
-    "name": "$1"
+    "name": "$name"
   }
 }
 EOF
 )
 
 starsd tx wasm execute $MINTER "$MSG" \
-  --amount 1000000000ustars \
+  --amount 100ustars \
   --gas-prices 0.025ustars --gas auto --gas-adjustment 1.9 \
   --from $TESTNET_KEY -b block -y -o json | jq .
  
