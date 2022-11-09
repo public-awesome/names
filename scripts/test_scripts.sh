ea0a3db354769fd7d97cbfa50a29f840ea3e1426
John Y <j@publicawesome.com>
2022-11-09 13:41:12 -0500

    test add wl and pub time

--- scripts/test_scripts.sh
+++ scripts/test_scripts.sh
@@ -46,3 +46,11 @@ bidder=$(starsd keys show $BIDDER | jq -r '.address')
 ./exec_accept_bid.sh $name $bidder
 
 # make new whitelist
+WL2=$(bash 05-init_wl.sh | jq -r '.logs[0].events[0].attributes[0].value')
+
+# add addresses to whitelist
+./exec_wl_add_addrs.sh '["stars1u5kav800kkkrzyvad67zhdmn4xajg6t5j7jm7k"]'
+
+# update public time
+TIME=$(date -v+1M +%s)
+./exec_update_public_time.sh "$(echo $TIME)000000000"
