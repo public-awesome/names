2a1bf39368b3f7da826c0cd5aaa2d444d9f84603
jason-c-child <child.jason@icloud.com>
2023-11-15 12:59:39 -0700

    update optimize script; stub more names test

--- typescript/packages/e2e-tests/tests/names/namesRenewal.test.ts
+++ typescript/packages/e2e-tests/tests/names/namesRenewal.test.ts
@@ -1,4 +1,4 @@
-import Context from '../setup/context'
+import Context, {CONTRACT_MAP} from '../setup/context'
 
 describe('Names Renewal', () => {
   let context: Context
@@ -8,6 +8,13 @@ describe('Names Renewal', () => {
     await context.initialize(true)
   });
 
+  test('is initialized', () => {
+    expect(context.getContractAddress(CONTRACT_MAP.MARKETPLACE)).toBeTruthy()
+    expect(context.getContractAddress(CONTRACT_MAP.NAME_MINTER)).toBeTruthy()
+    expect(context.getContractAddress(CONTRACT_MAP.SG721_NAME)).toBeTruthy()
+    expect(context.getContractAddress(CONTRACT_MAP.WHITELIST_UPDATABLE)).toBeTruthy()
+  })
+  
   test('STUB: true is true', () => {
     expect(true).toBe(true)
   })
