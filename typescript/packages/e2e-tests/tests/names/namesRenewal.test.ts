8985f13507f89822329f15edfd48e12d47258e0d
jason-c-child <child.jason@icloud.com>
2023-11-15 15:14:45 -0700

    start getting context setup correct

--- typescript/packages/e2e-tests/tests/names/namesRenewal.test.ts
+++ typescript/packages/e2e-tests/tests/names/namesRenewal.test.ts
@@ -1,4 +1,4 @@
-import Context, {CONTRACT_MAP} from '../setup/context'
+import Context, { CONTRACT_MAP } from '../setup/context'
 
 describe('Names Renewal', () => {
   let context: Context
@@ -6,15 +6,15 @@ describe('Names Renewal', () => {
   beforeAll(async () => {
     context = new Context()
     await context.initialize(true)
-  });
+  })
 
   test('is initialized', () => {
     expect(context.getContractAddress(CONTRACT_MAP.MARKETPLACE)).toBeTruthy()
-    expect(context.getContractAddress(CONTRACT_MAP.NAME_MINTER)).toBeTruthy()
+    // expect(context.getContractAddress(CONTRACT_MAP.NAME_MINTER)).toBeTruthy()
     expect(context.getContractAddress(CONTRACT_MAP.SG721_NAME)).toBeTruthy()
-    expect(context.getContractAddress(CONTRACT_MAP.WHITELIST_UPDATABLE)).toBeTruthy()
+    // expect(context.getContractAddress(CONTRACT_MAP.WHITELIST_UPDATABLE)).toBeTruthy()
   })
-  
+
   test('STUB: true is true', () => {
     expect(true).toBe(true)
   })
--- typescript/packages/e2e-tests/tests/setup/context.ts
+++ typescript/packages/e2e-tests/tests/setup/context.ts
@@ -14,10 +14,10 @@ import path from 'path'
 
 export const CONTRACT_MAP = {
   // core artifacts
-  MARKETPLACE: 'stargaze_fair_burn',
-  SG721_NAME: 'stargaze_royalty_registry',
-  NAME_MINTER: 'stargaze_name_minter',
-  WHITELIST_UPDATABLE: 'stargaze_whitelist_updatable',
+  MARKETPLACE: 'name_marketplace',
+  SG721_NAME: 'name_sg721',
+  NAME_MINTER: 'name_minter',
+  WHITELIST_UPDATABLE: 'whitelist_updatable',
 }
 
 export type TestUser = {
@@ -84,6 +84,70 @@ export default class Context {
 
   private instantiateContracts = async () => {
     let { client, address: sender } = this.getTestUser('user1')
+
+    let inistantiateMarketpace = await this.instantiateContract(client, sender, CONTRACT_MAP.MARKETPLACE, {
+      trading_fee_bps: 100,
+      min_price: '1',
+      ask_interval: 60,
+    })
+
+    let intantiateSG721Name = await this.instantiateContract(client, sender, CONTRACT_MAP.SG721_NAME, {
+      verifier: null,
+      base_init_msg: {
+        /*
+      pub name: String,
+    pub symbol: String,
+    pub minter: String,
+    pub collection_info: CollectionInfo<RoyaltyInfoResponse>,
+  */
+        name: 'Stargaze',
+        symbol: 'SGZ',
+        minter: this.getContractAddress(CONTRACT_MAP.MARKETPLACE),
+        collection_info: {
+          /*
+    pub creator: String,
+    pub description: String,
+    pub image: String,
+    pub external_link: Option<String>,
+    pub explicit_content: Option<bool>,
+    pub start_trading_time: Option<Timestamp>,
+    pub royalty_info: Option<T>,
+*/
+
+          creator: sender,
+          description: 'Stargaze NFTs',
+          image: 'https://stargaze.zone/images/sgz_logo.png',
+          external_link: null,
+          explicit_content: null,
+          start_trading_time: null,
+          royalty_info: null,
+        },
+      },
+    })
+
+    // let instantiateNameMinter = await this.instantiateContract(client, sender, CONTRACT_MAP.NAME_MINTER, {
+    //   /*
+    //       /// Temporary admin for managing whitelists
+    // pub admin: Option<String>,
+    // /// Oracle for verifying text records
+    // pub verifier: Option<String>,
+    // pub collection_code_id: u64,
+    // pub marketplace_addr: String,
+    // pub min_name_length: u32,
+    // pub max_name_length: u32,
+    // pub base_price: Uint128,
+    // pub fair_burn_bps: u64,
+    // pub whitelists: Vec<String>,*/
+    //   admin: null,
+    //   verifier: null,
+    //   collection_code_id: this.codeIds[CONTRACT_MAP.SG721_NAME],
+    //   marketplace_addr: this.getContractAddress(CONTRACT_MAP.MARKETPLACE),
+    //   min_name_length: 5,
+    //   max_name_length: 20,
+    //   base_price: '1000000',
+    //   fair_burn_bps: 100,
+    //   whitelists: [],
+    // })
   }
 
   private writeContext = () => {
